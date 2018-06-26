444fa1fc7e8b56aeae2b1868d964522e076c734f
Edith707 <edith.lopezhuerta@gmail.com>
2018-06-26 13:39:08 -0500

    subiendo cambios Angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Edith' },
+  { id: 12, name: 'Miguel' },
+  { id: 13, name: 'Juan' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Carlos' },
+
+];
